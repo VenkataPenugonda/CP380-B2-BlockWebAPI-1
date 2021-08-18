fdb5b0bd521619e259290839b71357087df70f54
Venkata Penugonda <penugondapavan01@gmail.com>
2021-08-18 11:59:06 -0400

    first commit

---
+++ CP380-B1-BlockList/Models/BlockList.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace CP380_B1_BlockList.Models
+{
+    public class BlockList
+    {
+        public IList<Block> Chain { get; set; }
+
+        public int Difficulty { get; set; } = 2;
+
+        public BlockList()
+        {
+            Chain = new List<Block>();
+            MakeFirstBlock();
+        }
+
+        public void MakeFirstBlock()
+        {
+            var block = new Block(DateTime.Now, null, new List<Payload>());
+            block.Mine(Difficulty);
+            Chain.Add(block);
+        }
+
+        public void AddBlock(Block block)
+        {
+            // TODO - Done
+            var block1 = new Block(block.TimeStamp, block.PreviousHash, block.Data);
+            block1.Mine(Difficulty);
+            Chain.Add(block1);
+        }
+
+        public bool IsValid()
+        {
+            // TODO
+            for (int i = 1; i < Chain.Count; i++)
+            {
+                Block prevVal = Chain[i - 1];
+                Block curtVal = Chain[i];
+                if (curtVal.Hash != curtVal.CalculateHash())
+                    return false;
+                if (curtVal.PreviousHash != prevVal.Hash)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
--- CP380-B2-BlockWebAPI/Controllers/PendingPayloadsController.cs
+++ CP380-B2-BlockWebAPI/Controllers/PendingPayloadsController.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CP380_B1_BlockList.Models;
+using CP380_B2_BlockWebAPI.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace CP380_B2_BlockWebAPI.Controllers
@@ -13,5 +16,23 @@ namespace CP380_B2_BlockWebAPI.Controllers
     public class PendingPayloadsController : ControllerBase
     {
         // TODO
+        [Route("/PendingPayloads")]
+        [HttpGet]
+        public IActionResult Get()
+        {
+            List<Payload> data = new()
+            {
+                new Payload("user", TransactionTypes.GRANT, 30, null),
+                new Payload("user", TransactionTypes.BUY, 8, "item"),
+            };
+            return Ok(data);
+        }
+
+        [Route("/PendingPayloads")]
+        [HttpPost]
+        public IActionResult Post(string user, TransactionTypes t, int amount, string item)
+        {
+            return Ok(new Payload(user, t, amount, item));
+        }
     }
 }
